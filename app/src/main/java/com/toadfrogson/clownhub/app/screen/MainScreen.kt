ed6354cd8d14e243ad9ed6744ecd6992a3f02885
wimpytoad <leshaterentev@gmail.com>
2024-02-10 17:10:55 +0100

    Add Snackbar events

--- app/src/main/java/com/toadfrogson/clownhub/app/screen/MainScreen.kt
+++ app/src/main/java/com/toadfrogson/clownhub/app/screen/MainScreen.kt
@@ -1,36 +1,57 @@
 package com.toadfrogson.clownhub.app.screen
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.toadfrogson.clownhub.app.viewmodel.MainViewModel
+import com.toadfrogson.clownhub.app.viewmodel.UiEvents
 import com.toadfrogson.clownhub.ui.components.ClownLoader
 import com.toadfrogson.clownhub.ui.components.JokesList
 import kotlinx.coroutines.Dispatchers
 import org.koin.androidx.compose.koinViewModel
 
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MainScreen(viewModel: MainViewModel = koinViewModel()) {
     LaunchedEffect(viewModel, Dispatchers.IO) {
         viewModel.loadContent()
     }
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        val state by viewModel.uiState.collectAsState()
-        val isLoading = state.isLoading
+    val snackbarHostState = remember { SnackbarHostState() }
 
-        if (isLoading) {
-            ClownLoader()
-        } else {
-            JokesList(content = state.content)
+    Scaffold(
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+        ) {
+            val events = remember { viewModel.uiEvents }
+            LaunchedEffect(events) {
+                events.collect {
+                    if (it is UiEvents.SnackbarEvent) {
+                        snackbarHostState.showSnackbar(message = it.message)
+                    }
+                }
+            }
+            val state by viewModel.uiState.collectAsState()
+            val isLoading = state.isLoading
+
+            if (isLoading) {
+                ClownLoader()
+            } else {
+                JokesList(content = state.content)
+            }
         }
     }
 }
--- app/src/main/java/com/toadfrogson/clownhub/app/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/toadfrogson/clownhub/app/viewmodel/MainViewModel.kt
@@ -3,28 +3,40 @@ package com.toadfrogson.clownhub.app.viewmodel
 import androidx.lifecycle.ViewModel
 import com.toadfrogson.clownhub.data.model.JokeModel
 import com.toadfrogson.clownhub.data.repo.JokesRepo
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
 data class MainState(val isLoading: Boolean = false, val content: List<JokeModel> = emptyList())
 
 class MainViewModel(private val repo: JokesRepo): ViewModel() {
-    private val privateUiState = MutableStateFlow(MainState())
-    val uiState: StateFlow<MainState> = privateUiState.asStateFlow()
+    private val _uiState = MutableStateFlow(MainState())
+    val uiState: StateFlow<MainState> = _uiState.asStateFlow()
+
+    private val _uiEvents = MutableSharedFlow<UiEvents>()
+    val uiEvents: SharedFlow<UiEvents> = _uiEvents.asSharedFlow()
+
 
     suspend fun loadContent() {
-        privateUiState.update {
+        _uiState.update {
             it.copy(isLoading = true)
         }
         val result = repo.fetchJokes()
+
         if (result.isSuccess && result.getOrNull() != null) {
-            privateUiState.update {
+            _uiState.update {
                 it.copy(isLoading = false, content = result.getOrNull()?: emptyList())
             }
         } else {
-
+            _uiEvents.emit(UiEvents.SnackbarEvent("Failed to Load", true))
         }
     }
+}
+
+sealed class UiEvents {
+    data class SnackbarEvent(val message: String, val errorSnackbar: Boolean): UiEvents()
 }
